87ca8882ccb7fea539f7c3812991615b9d647cfd
Alex <ajhollid@gmail.com>
2018-09-05 18:19:03 -0700

    Set up canvas

--- main.js
+++ main.js
@@ -2,4 +2,10 @@ const links = [];
 const nodes = [];
 const transactions = [];
 const radius = 3;
+const txlimit = 10;
 
+const canvas = document.querySelector('canvas');
+const context = canvas.getContext('2d');
+canvas.width = window.innerWidth;
+canvas.height = window.innerHeight;
+const { width, height } = canvas;
